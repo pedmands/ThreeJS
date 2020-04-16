4c3f6adc77cdafd0aa32e6f77c741c254a77e551
pedmands <pedmands@gmail.com>
2020-04-16 00:45:43 -0400

    Added some comments

--- main.js
+++ main.js
@@ -5,30 +5,31 @@ function init() {
     var plane = getPlane(4);
 
     // Position the box on the ground
-    box.position.y = box.geometry.parameters.height/2;
+    box.position.y = box.geometry.parameters.height / 2;
     // Three.js uses radians
-    plane.rotation.x = Math.PI/2;
+    plane.rotation.x = Math.PI / 2;
 
     scene.add(box);
     scene.add(plane);
 
     var camera = new THREE.PerspectiveCamera(
         45,
-        window.innerWidth/window.innerHeight,
+        window.innerWidth / window.innerHeight,
         1,
         1000
     );
 
+    // Move camera away from the scene origin
     camera.position.x = 1;
     camera.position.y = 2;
     camera.position.z = 5;
 
+    // Look at the origin
     camera.lookAt(new THREE.Vector3(0, 0, 0));
-    
 
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    document.getElementById('webgl').appendChild(renderer.domElement);
+    document.getElementById("webgl").appendChild(renderer.domElement);
     update(renderer, scene, camera);
 
     return scene;
@@ -37,12 +38,9 @@ function init() {
 function getBox(w, h, d) {
     var geometry = new THREE.BoxGeometry(w, h, d);
     var material = new THREE.MeshBasicMaterial({
-        color: 0x00f6fa
+        color: 0x00f6fa,
     });
-    var mesh = new THREE.Mesh(
-        geometry,
-        material
-    );
+    var mesh = new THREE.Mesh(geometry, material);
 
     return mesh;
 }
@@ -51,22 +49,17 @@ function getPlane(size) {
     var geometry = new THREE.PlaneGeometry(size, size);
     var material = new THREE.MeshBasicMaterial({
         color: 0xf2ff00,
-        side: THREE.DoubleSide
+        side: THREE.DoubleSide,
     });
-    var mesh = new THREE.Mesh(
-        geometry,
-        material
-    );
+    var mesh = new THREE.Mesh(geometry, material);
 
     return mesh;
 }
 
 function update(renderer, scene, camera) {
-    renderer.render(
-        scene,
-        camera
-    );
-    requestAnimationFrame(function() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(function () {
+        // Recursively call update to continue grabbing the new animation frames
         update(renderer, scene, camera);
     });
 }
